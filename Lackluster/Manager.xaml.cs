8873818320fbba0ac0471e24b75697b7956edb8f
wayneparkerkent <curtjamesdixon@gmail.com>
2018-03-22 22:33:28 -0500

    Rental Return Scanning

--- Lackluster/Manager.xaml.cs
+++ Lackluster/Manager.xaml.cs
@@ -38,18 +38,13 @@ namespace Lackluster
             this.Close();
         }
 
-        private void btnReturnScan_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void btnStartRental_Click(object sender, RoutedEventArgs e)
         {
-            //Show the txtScanEntry Box
-            txtScanEntry.Visibility = Visibility.Visible;
+            //Show the txtRentalEntry Box
+            txtRentalEntry.Visibility = Visibility.Visible;
 
-            //Set focus to the txtScanEntry Box
-            txtScanEntry.Focus();
+            //Set focus to the txtRentalEntry Box
+            txtRentalEntry.Focus();
         }
 
         private void txtScanEntry_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -64,7 +59,7 @@ namespace Lackluster
                 foreach (Movie i in lstRent.Items)
                 {
                     //Determine if the upc entered is already in the list
-                    if (i.upc.ToString().Equals(txtScanEntry.Text))
+                    if (i.upc.ToString().Equals(txtRentalEntry.Text))
                     {
                         //Set found to true since the movie is already in the list
                         found = true;
@@ -80,7 +75,7 @@ namespace Lackluster
                 {
 
                     //Create a movie object by passing the scanned text
-                    Movie scannedEntry = new Movie(txtScanEntry.Text);
+                    Movie scannedEntry = new Movie(txtRentalEntry.Text);
 
                     //Check if the movie is actually a movie in our database
                     if (scannedEntry.isMovieCheck())
@@ -99,19 +94,87 @@ namespace Lackluster
                     txtTotal.Text = String.Format("{0:C}", total);
                 }
 
-                //Reset the txtScanEntry box
-                txtScanEntry.Text = "";
+                //Reset the txtRentalEntry box
+                txtRentalEntry.Text = "";
             }
 
-            //TODO - Calculate Total cost
+            //TODO - Check that the movie is actually in stock (not rented out)
         }
 
         private void btnCompleteRental_Click(object sender, RoutedEventArgs e)
         {
-            //Hide the txtScanEntry Box
-            txtScanEntry.Visibility = Visibility.Hidden;
+            //Hide the txtRentalEntry Box
+            txtRentalEntry.Visibility = Visibility.Hidden;
 
             //TODO - Complete the Rental Process
+        }
+
+        private void btnStartReturn_Click(object sender, RoutedEventArgs e)
+        {
+            //Show the txtReturnEntry Box
+            txtReturnEntry.Visibility = Visibility.Visible;
+
+            //Set focus to the txtReturnEntry Box
+            txtReturnEntry.Focus();
+        }
+
+        private void txtReturnEntry_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            //Look for the Return key press
+            if (e.Key == Key.Return)
+            {
+                //Variable to hold whether movie is already in the list
+                bool found = false;
+
+                //Search each list entry
+                foreach (Movie i in lstReturn.Items)
+                {
+                    //Determine if the upc entered is already in the list
+                    if (i.upc.ToString().Equals(txtReturnEntry.Text))
+                    {
+                        //Set found to true since the movie is already in the list
+                        found = true;
+                    }
+                }
+
+                //Check if the movie is already in the list
+                if (found)
+                {
+                    MessageBox.Show("Movie already added");
+                }
+                else
+                {
+
+                    //Create a movie object by passing the scanned text
+                    Movie scannedEntry = new Movie(txtReturnEntry.Text);
+
+                    //Check if the movie is actually a movie in our database
+                    if (scannedEntry.isMovieCheck())
+                    {
+                        //Add the movie object to the list
+                        lstReturn.Items.Add(scannedEntry);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("This is not an active movie");
+                    }
+
+                }
+
+                //Reset the txtRentalEntry box
+                txtReturnEntry.Text = "";
+            }
+
+            //TODO - Check to make sure the movie is indeed rented out
+        }
+
+        private void btnCompleteReturn_Click(object sender, RoutedEventArgs e)
+        {
+            //Hide the txtReturnEntry Box
+            txtReturnEntry.Visibility = Visibility.Hidden;
+
+            //TODO - Complete the Return Process
         }
     }
 }
